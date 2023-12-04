4546f36fbaf60ed63c1a9d497e2133351f4b8278
shivamdinde <shivamdinde60@gmail.com>
2023-12-05 00:20:10 +0530

    Initial commit

---
+++ CourseHibernateProject/src/main/java/day6/utils/HibernateUtils.java
@@ -0,0 +1,13 @@
+package day6.utils;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtils {
+	public static SessionFactory getSessionFactory() {
+		Configuration hibernateConfig = new Configuration();
+		hibernateConfig=hibernateConfig.configure();
+		SessionFactory hibernateFactory = hibernateConfig.buildSessionFactory();
+		return hibernateFactory;
+	}
+}
